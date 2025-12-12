9919e268dd13e9efd2e4bf0ff7657a28ebd999db
sertnayong <nuannimprasert@gmail.com>
2025-12-12 22:01:23 +0700

    edit redirect

--- src/app/redirect/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-import { auth } from "@/server/services/auth/authService";
-import { resolveRoleRedirectPath } from "@/lib/auth/accessRole";
-
-export default async function Redirect() {
-  const session = await auth();
-  const targetPath = resolveRoleRedirectPath(session?.user?.role ?? undefined);
-  redirect(targetPath);
-}
