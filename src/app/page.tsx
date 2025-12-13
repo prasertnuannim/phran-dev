2cea2ed7c177984132ae7e2b98356e9cfdaa99e1
sertnayong <nuannimprasert@gmail.com>
2025-12-13 13:29:39 +0700

    try session2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-"use client";
-
 import Hero from "@/components/sections/Hero";
 import About from "@/components/sections/About";
 import Contact from "@/components/sections/Contact";
 import { auth } from "@/server/services/auth/authService";
 
 export default async function Page() {
-    const session = await auth();
+  const session = await auth();
   console.log("can access to main page >>> ", session);
   return (
     <main className="snap-y snap-mandatory overflow-y-auto scroll-smooth">
