3130fc39ac863c5a03ad3b58722184f5e8f4e310
sertnayong <nuannimprasert@gmail.com>
2025-12-13 23:07:39 +0700

    remove redirect

--- src/app/(auth)/redirect/page.tsx
+++
@@ -1,8 +0,0 @@
-// import { redirect } from "next/navigation";
-// import { auth, resolveRoleRedirectPath } from "@/server/services/auth/authService";
-
-// export default async function Redirect() {
-//   const session = await auth();
-//   const targetPath = resolveRoleRedirectPath(session?.user?.role ?? undefined);
-//   redirect(targetPath);
-// }
