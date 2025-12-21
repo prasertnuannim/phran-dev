9837db854a65818eb333750e4f943a11c975ea44
sertnayong <nuannimprasert@gmail.com>
2025-12-22 00:20:57 +0700

    up home page

--- src/app/(admin)/statistics/daily/page.tsx
+++ src/app/(admin)/statistics/daily/page.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+export const dynamic = "force-dynamic";
 
-export default function page() {
-  return (
-    <div>Daily Page</div>
-  )
+export default function Page() {
+  return <div>Daily Page</div>;
 }
